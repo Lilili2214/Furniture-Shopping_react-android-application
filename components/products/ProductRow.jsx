5f48c8d437b3a7026b94905fa18adb3341d439e4
Lilili2214 <lizz2292002@gmail.com>
2024-04-24 00:29:41 +0700

    productcart and productdetail

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { FlatList, Text, View } from 'react-native'; // Import FlatList and other components
 import { SIZES } from '../../constants';
 import ProductCartView from './ProductCartView';
+import styles from './productRow.style';
 
 const ProductRow = () => {
   const products = [1, 2, 3,4];
 
   return (
-    <View style={{marginTop: SIZES.medium}}>
+    <View style={styles.container}>
     <FlatList
       data={products}
       renderItem={({ item }) => <ProductCartView/>} 
--- screens/ProductDetail.jsx
+++ screens/ProductDetail.jsx
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , Image} from 'react-native'
 import React from 'react'
 
-const ProductDetail = () => {
+import {Ionicons} from "@expo/vector-icons"
+import styles from './productDetail.style'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+const ProductDetail = ({navigation}) => {
   return (
-    <View>
-      <Text>ProductDetail</Text>
+    <View style={styles.container}>
+        <View style= {styles.upperRow}>
+            <TouchableOpacity onPress={()=>navigation.goBack()}>
+            <Ionicons name='chevron-back-circle' size={30}/>
+
+            </TouchableOpacity>
+            
+
+            <TouchableOpacity onPress={()=>{}}>
+            <Ionicons name='heart' size={30}/>
+            
+
+            </TouchableOpacity>
+        </View>
+        <Image
+            source={{uri: "https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_640.jpg"}}
+            style={styles.image}
+        />
+        <View style={styles.details}>
+            <View style={styles.titleRow}>
+                <Text style={styles.title}>Product</Text>
+                <View style={styles.priceWrapper}>
+                    <Text style={styles.price}>$ 299</Text>
+                </View>
+            
+            
+            </View>
+            <View style={styles.ratingRow}>
+                <View style={styles.rating}>
+                    {[1,2,3,4,5].map((index)=>(
+                        <Ionicons 
+                        key={index}
+                        name='star'
+                        size={24}
+                        color="gold"/>
+                    ))}
+                </View>
+            </View>
+        </View>
     </View>
   )
 }
 
 export default ProductDetail
 
-const styles = StyleSheet.create({})
